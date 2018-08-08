998e7ee817fe961ec0c42b59db3b53dbe0f8db20
sunnyhitha <41725679+sunnyhitha@users.noreply.github.com>
2018-08-08 14:01:37 +0530

    Create combine.java

---
+++ combine.java
@@ -0,0 +1,17 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{   
+		Scanner sc=new Scanner(System.in);
+		String s1=sc.next();
+		String s2=sc.next();
+		System.out.print(s1+s2);// your code goes here
+	}
+}
